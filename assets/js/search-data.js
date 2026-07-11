94b1b73e2ee4f9e426312b567d795a1d4e13a1c8
Kenaz123 <127205285+Kenaz123@users.noreply.github.com>
2026-07-11 16:05:35 +0000

    Deploying to gh-pages from @ Kenaz123/Kenaz123.github.io@4d74694d29479155e0751362fbdcf54bfdaf8829 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,7 +44,18 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-hello-welcome-to-my-homepage",
+            },{id: "post-graduation-memory-in-nju",
+        
+          title: "Graduation Memory in NJU",
+        
+        description: "Looking back on four years at Nanjing University before heading to UIUC.",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2026/graduation-memory-in-nju/";
+          
+        },
+      },{id: "post-hello-welcome-to-my-homepage",
         
           title: "Hello, welcome to my homepage",
         
